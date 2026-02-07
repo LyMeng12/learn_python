f77a0969ea281103233d1d0e881c29ecc9c6a684
LyMeng12 <mengsaoly@gmail.com>
2026-02-08 00:49:16 +0700

    Python Variables assign Multiple Values

--- Frist.py
+++ Frist.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+# This is Python Variables assign Multiple Values
+a,b,c="men","kaka","ags"
+print(a,b,c)
+
+a=b=c ="Banana"
+print(a,b,c)
+
+name=["men","ajg","man"]
+a,d,f=name
+print(a,d,f)
