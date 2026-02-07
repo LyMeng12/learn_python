25b44f59199bfa88c27ee2f23a63cd452d9ffe1b
LyMeng12 <mengsaoly@gmail.com>
2026-02-08 01:00:47 +0700

    Python Global Variables

--- Frist.py
+++ Frist.py
@@ -1,10 +1,40 @@
-# This is Python Variables assign Multiple Values
-a,b,c="men","kaka","ags"
-print(a,b,c)
+# This is Python Global Variables.
+# Global variables can be used by everyone, both inside of functions and outside.
 
-a=b=c ="Banana"
-print(a,b,c)
+# not used global
+# Ex1
+# x="NaRA"
+# def myfun():
+#     print("Holle "+x)
+# myfun()
 
-name=["men","ajg","man"]
-a,d,f=name
-print(a,d,f)
+# Ex2
+# a = "awesome"
+# def myfunc():
+#   a = "fantastic"
+#   print("Python is " + a)
+#
+# myfunc()
+#
+# print("Python is "+a)
+
+# when used Global
+# Ex1
+# def myfunc():
+#   global x
+#   x = "fantastic"
+#
+# myfunc()
+#
+# print("Python is " + x)
+
+# Ex2
+# x = "awesome"
+#
+# def myfunc():
+#   global x
+#   x = "fantastic"
+#
+# myfunc()
+#
+# print("Python is " + x)
