707b2811fee68b3426ea58384cab286b08e482da
LyMeng12 <mengsaoly@gmail.com>
2026-02-08 10:04:07 +0700

    Python Slicing Strings

--- Frist.py
+++ Frist.py
@@ -1,40 +1,9 @@
-# This is Python Global Variables.
-# Global variables can be used by everyone, both inside of functions and outside.
-
-# not used global
-# Ex1
-# x="NaRA"
-# def myfun():
-#     print("Holle "+x)
-# myfun()
-
-# Ex2
-# a = "awesome"
-# def myfunc():
-#   a = "fantastic"
-#   print("Python is " + a)
+# Python - Slicing Strings
+# strip() គឺជា function របស់ string ក្នុង Python
+# វាធ្វើការ លុបចន្លោះ (space), tab (\t), newline (\n) នៅ
 #
-# myfunc()
+# ខាងមុខ (left side)
 #
-# print("Python is "+a)
-
-# when used Global
-# Ex1
-# def myfunc():
-#   global x
-#   x = "fantastic"
-#
-# myfunc()
-#
-# print("Python is " + x)
-
-# Ex2
-# x = "awesome"
-#
-# def myfunc():
-#   global x
-#   x = "fantastic"
-#
-# myfunc()
-#
-# print("Python is " + x)
+# ខាងក្រោយ (right side)
+hi=" jello,world! "
+print(hi.split())
