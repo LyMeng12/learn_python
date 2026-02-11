d9c079109d43b5b87a865e98f73efa17bfdd22d0
LyMeng12 <mengsaoly@gmail.com>
2026-02-11 13:58:54 +0700

    Python String

--- Frist.py
+++ Frist.py
@@ -1,8 +1,35 @@
-# Python Casting
-# Casting = convert all variable.
+a = 'Hello'
+b = "Hello"
 
-i=1
-print(type(i))
-s=float(i)
-print(type(s))
+print(a)
+print(b)
+print(type(a), type(b))
 
+# String
+name = "Meng"
+print(f'Hello {name}')
+print(f"Hello {name}")
+
+# Slixing String
+b = "Hello, World!"
+print(b[-7:-2])
+
+# Format String
+s=67
+txt=f"I'm {s} years old"
+print(txt)
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
+# Escape Characters
+# Code	================ Result
+# \'      ================ Single Quote
+# \\      ================ Backslash
+# \n      ================ New Line
+# \r	    ================ Carriage Return
+# \t	    ================ Tab
+# \b	    ================ Backspace
+# \f	    ================ Form Feed
+# \ooo	================ Octal value
+# \xhh	================ Hex value
