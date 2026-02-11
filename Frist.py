f6c51a9a46b3f8d009d85bc0d182d5a78ba59000
LyMeng12 <mengsaoly@gmail.com>
2026-02-11 13:31:49 +0700

    Python Casting

--- Frist.py
+++ Frist.py
@@ -1,14 +1,8 @@
-# Python Data Types
+# Python Casting
+# Casting = convert all variable.
 
-# x = 1j	complex
-# x = ["apple", "banana", "cherry"]	list
-# x = ("apple", "banana", "cherry")	tuple
-# x = range(6)	range
-# x = {"name" : "John", "age" : 36}	dict
-# x = {"apple", "banana", "cherry"}	set
-# x = frozenset({"apple", "banana", "cherry"})	frozenset
-# x = True	bool
-# x = b"Hello"	bytes
-# x = bytearray(5)	bytearray
-# x = memoryview(bytes(5))	memoryview
-# x = None	NoneType
+i=1
+print(type(i))
+s=float(i)
+print(type(s))
+
