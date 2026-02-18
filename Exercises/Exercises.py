f5bf0421b60ce0e3f7cb3322701f1f4f8471f921
LyMeng12 <mengsaoly@gmail.com>
2026-02-18 23:33:03 +0700

    Exercise used Tuple

--- Exercises/Exercises.py
+++ Exercises/Exercises.py
@@ -1,2 +1,42 @@
-student=("meng","lyly","nara","kaka","jana")
-print("School have ",len(student),"Students.")
+# Exercise3
+# student=("meng","lyly","nara","kaka","jana")
+# print("School have ",len(student),"Students.")
+
+# Exercises 4
+# student =("meng","lyly","nara","kaka","jana")
+# for name in student:
+#     print("Student Name: ",name)
+
+# Exercises 5
+# country=("Cambodia","Vietnam","Laos","China")
+# newCountry=country*2
+# print(newCountry)
+
+# Exercises 6
+# number=(1,2,3,4,5,6)
+# y=list(number) # convert number to list send to y
+# y[2]=10 # change index 2=3 to 10
+# number=tuple(y) #convert y to tuple send to number
+# print(number)
+
+# Exercises 7
+# This my Idea
+# tulpe=("meng",21,"Male")
+# name=str(tulpe[0])
+# age=int(tulpe[1])
+# gender=str(tulpe[2])
+# print("Your Name: ",type(name))
+# print("Your Age: ",type(age))
+# print("Your Gender: ",type(gender))
+
+# (name,age,gender)=tulpe
+# print(tulpe)
+# print("Your Name: ",type(name))
+# print("Your Age: ",type(age))
+# print("Your Gender: ",type(gender))
+
+# Exercises 8
+
+# Exercises 9
+
+# Exercises 10
