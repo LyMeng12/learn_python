7758a8b82a134f931f2c6f9e9c4cb5dcc2b5bbf3
LyMeng12 <mengsaoly@gmail.com>
2026-02-20 15:24:08 +0700

    projuct banking 10% done admin and customer

--- Projuct1/Projuct.py
+++ Projuct1/Projuct.py
@@ -10,7 +10,7 @@ class Banking:
             'Age':age,
             'Password':password,
             'Phone':phone,
-            'Role':role,
+            'role':role,
             'Money':money
         }
         self.User.append(Users)
@@ -18,58 +18,76 @@ class Banking:
         print(f"Added User {Name} successfully")
     def login(self,name,password):
         for user in self.User:
-            if user['name']==name and user['password']==password:
+            if user['name']==name and user['Password']==password:
                 if user['role']=='admin':
                     return "admin"
                 elif user['role']=='customer':
                     return "customer"
-        return print("User not found!")
+        return "User not found!"
+
+
+
+def customer():
+    print("=====Welcome Customer=====")
+
+def admin():
+    print("=====Welcome Admin=====")
 
 def main():
-        bank=Banking()
-        while True:
-            print("=====Welcome to Banking=====")
-            print()
-            try:
-                print("1. Login User")
-                print("2. Register User")
-                print("3. Exit")
-                choice=int(input("Enter your choice: "))
-                if choice==1:
-                    print("=====Welcome to From Login User=====")
-                    name=input("Enter your name: ")
-                    password=input("Enter your password: ")
-                    if bank.login(name,password)=="admin":
-                        print("=====Welcome Administrator=====")
-                    elif bank.login(name,password)=="customer":
-                        print("=====Welcome Customer=====")
+    bank = Banking()
+    while True:
+        print("=====Welcome to Banking=====")
+        print()
+        try:
+            print("1. Login User")
+            print("2. Register User")
+            print("3. Exit")
+            choice = int(input("Enter your choice: "))
+            if choice == 1:
+                print("=====Welcome to From Login User=====")
+                while True:
+                    name = input("Enter your name: ")
+                    password = input("Enter your password: ")
+                    user = bank.login(name, password)
+                    if user == "admin":
+                        admin()
+
+                    elif user == "customer":
+                        customer()
+
                     else:
-                        bank.login(name,password)
-                elif choice==2:
-                    print("=====Welcome to Register User=====")
-                    name=input("Enter your name: ")
-                    print("Choose Your Gender.")
-                    print("1.Male/2.Female")
-                    gen=int(input("Enter your gender: "))
-                    if gen==1:
-                        gender="Male"
-                    elif gen==2:
-                        gender="Female"
-                    age=input("Enter your age: ")
-                    password=input("Enter your password: ")
-                    phone=input("Enter your phone: ")
-                    role="customer"
-                    money=input("Enter your money: ")
-
-                elif choice==3:
-                    break
-            except ValueError:
-                print("Please enter a Number choice.")
+                        print(user)
 
 
+            elif choice == 2:
+                print("=====Welcome to Register User=====")
+                name = input("Enter your name: ")
+                print("Choose Your Gender.")
+                print("1.Male/2.Female")
+                gen = int(input("Enter your gender: "))
+                if gen == 1:
+                    gender = "Male"
+                elif gen == 2:
+                    gender = "Female"
+                age = int(input("Enter your age: "))
+                password = input("Enter your password: ")
+                phone = input("Enter your phone: ")
+                role = "customer"
+                money = float(input("Enter your money: "))
+                try:
+                    bank.addUser(name, gender, age, password, phone, role, money)
+                except ValueError:
+                    print("Create not successfully!")
 
+            elif choice == 3:
+                break
+        except ValueError:
+            print("Please enter a Number choice.")
 if __name__ == "__main__":
     main()
+
+
+
 
 
 
